a9194a1923bbe292a3811509cf6a2d0552bb7020
WAYLIIKE <valentynsavitskyy@gmail.com>
2024-09-28 04:10:08 +0200

    update base configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import FirstPage from 'pages/FirstPage/FirstPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
+import HomePage from './pages/HomePage/HomePage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route path="/first" element={<FirstPage />} />
+        <Route path="/first" element={<HomePage />} />
         <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
