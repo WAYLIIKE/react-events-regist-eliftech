ff5c199c8d8de8e6e98279d30747572a483a295d
WAYLIIKE <valentynsavitskyy@gmail.com>
2024-09-29 21:32:00 +0200

    update lists of participants and events

--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -1,11 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { Container } from '../../components/Container/Container';
 import { Event } from '../../components/Event/Event';
+import { useSelector } from 'react-redux';
+import { selectLoading } from '../../redux/selectors';
 
 // import css from './EventPage.module.css';
 
 const EventPage = () => {
   const { eventId } = useParams();
+  const isLoading = useSelector(selectLoading);
 
   return (
     <div>
