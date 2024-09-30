bfe0449518aa2bc125a3ac25a1580cc5668bff85
WAYLIIKE <valentynsavitskyy@gmail.com>
2024-09-30 16:36:42 +0200

    update participants list

--- src/components/ParticipantsListItem/ParticipantsListItem.jsx
+++ src/components/ParticipantsListItem/ParticipantsListItem.jsx
@@ -3,7 +3,12 @@ import css from './ParticipantsListItem.module.css';
 export const ParticipantsListItem = ({ participant }) => {
   return (
     <li className={css.item}>
-      <p>{participant.name}</p>
+      <div className={css.logoWrapper}>
+        <div className={css.logo}>
+          <p className={css.logoWord}>{participant.name[0]}</p>
+        </div>
+        <p>{participant.name}</p>
+      </div>
     </li>
   );
 };
